80602e803f9663cf5eed81460d30463fead489d7
Shubham-Singh1810 <hittheshubham1810@gmail.com>
2024-09-03 18:55:26 +0530

    after adding statics api

--- src/controller/adminController.js
+++ src/controller/adminController.js
@@ -7,49 +7,58 @@ require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 const cloudinary = require("../utils/cloudinary");
 const jwt = require("jsonwebtoken");
 const upload = require("../utils/multer");
+const Member = require("../model/memberSchema");
+const moment = require("moment");
 
-adminController.post("/createAdmin", upload.single("photo"), async (req, res) => {
-  try {
-    const existingAdmin = await Admin.findOne({
-      $or: [{ phoneNo: req.body.phoneNo }],
-    });
+adminController.post(
+  "/createAdmin",
+  upload.single("photo"),
+  async (req, res) => {
+    try {
+      const existingAdmin = await Admin.findOne({
+        $or: [{ phoneNo: req.body.phoneNo }],
+      });
 
-    if (existingAdmin) {
-      return res.status(409).send({
+      if (existingAdmin) {
+        return res.status(409).send({
+          success: false,
+          message: "mobile number already exists",
+        });
+      }
+
+      let adminData = { ...req.body };
+
+      if (req.file) {
+        let photo = await cloudinary.uploader.upload(
+          req.file.path,
+          function (err, result) {
+            if (err) {
+              return err;
+            } else {
+              return result;
+            }
+          }
+        );
+        adminData = { ...req.body, photo: photo.url };
+      }
+
+      const adminCreated = new Admin(adminData);
+      await adminCreated.save();
+
+      sendResponse(res, 200, "Success", {
+        success: true,
+        message: "Admin Registered successfully!",
+        adminData: adminCreated,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
         success: false,
-        message: "mobile number already exists",
+        message: error.message || "Internal server error",
       });
     }
-
-    let adminData = { ...req.body };
-
-    if (req.file) {
-      let photo = await cloudinary.uploader.upload(req.file.path, function (err, result) {
-        if (err) {
-          return err;
-        } else {
-          return result;
-        }
-      });
-      adminData = { ...req.body, photo: photo.url };
-    }
-
-    const adminCreated = new Admin(adminData);
-    await adminCreated.save();
-
-    sendResponse(res, 200, "Success", {
-      success: true,
-      message: "Admin Registered successfully!",
-      adminData: adminCreated,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      success: false,
-      message: error.message || "Internal server error",
-    });
   }
-});
+);
 
 adminController.post("/login", async (req, res) => {
   try {
@@ -93,11 +102,57 @@ adminController.get("/getAdmin", async (req, res) => {
 
 adminController.put("/updateAdminData", async (req, res) => {
   try {
-    const data = await adminServices.updateData({ _id: req.body._id }, req.body);
+    const data = await adminServices.updateData(
+      { _id: req.body._id },
+      req.body
+    );
     sendResponse(res, 200, "Success", {
       success: true,
       message: "Admin Updated successfully!",
       data: data,
+    });
+  } catch (error) {
+    console.log(error);
+    sendResponse(res, 500, "Failed", {
+      message: error.message || "Internal server error",
+    });
+  }
+});
+
+adminController.get("/statics", async (req, res) => {
+  try {
+    // Count total number of members
+    const memberCount = await Member.countDocuments();
+    const members = await Member.find({});
+    const currentDateStr = moment().format("DD-MM-YYYY");
+    function compareDates(date1, date2) {
+      // Parse the dates using moment
+      const d1 = moment(date1, "DD-MM-YYYY");
+      const d2 = moment(date2, "DD-MM-YYYY");
+  
+      // Compare the two dates
+      if (d1.isAfter(d2)) {
+          return true
+      } else if (d1.isBefore(d2)) {
+          return false
+      } else {
+          return false
+      }
+  }
+    const pendingPaymentMember = members?.filter((v, i)=>{
+      const dueDate = moment(v?.dueDate).format("DD-MM-YYYY")
+      if(compareDates(currentDateStr, dueDate)){
+        return v
+      }
+    })
+    sendResponse(res, 200, "Success", {
+      success: true,
+      message: "Admin dashboard statistics fetched successfully!",
+      data: {
+        memberCount,
+        pendingPaymentsCount: pendingPaymentMember.length,
+        pendingPayments: pendingPaymentMember,
+      },
     });
   } catch (error) {
     console.log(error);
--- src/model/notificationSchema.js
+++ src/model/notificationSchema.js
@@ -1,24 +1,11 @@
 const mongoose = require("mongoose");
 const timestamps = require("mongoose-timestamp");
-const { type } = require("os");
 
 const notificationSchema = mongoose.Schema({
-    photo: { type: String },
-    fullName: { type: String },
-    mobile: { type: String },
-    address: { type: String },
-    profession: { type: String },
-    bodyWeight: { type: String },
-    fitnessGoal: {
-        type: [String],
-        enum: ['WeightLoss', 'WeightGain', 'FatLoss']
-    },
-    package: {
-        type: [String],
-        enum: ['1Month', '3Month', '6Month', '12Month']
-    },
-    dueDate: { type: String },
-    
+  message: { type: String },
+  type: { type: [String], enum: ["member", "enquiry"]},
+  isRead: { type: Boolean },
+  releventId: { type: String },
 });
 
 notificationSchema.plugin(timestamps);
